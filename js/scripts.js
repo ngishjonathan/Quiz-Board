40ff91320a65da4512454287fecdfb1494519ca7
ngishjonathan <balozimwash@gmail.com>
2019-04-01 15:17:24 +0300

    new updates on the javascript and assigning new classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $("#add").submit(function(event) {
     var shortform=parseInt($("input:radio[name=Q1]:checked").val());
-    var js=parseInt($("input:radio[name=Q2]:checked").val());
+    var java=parseInt($("input:radio[name=Q2]:checked").val());
     var language=parseInt("input:radio[name=Q3]:checked").val());
     var mwas=parseInt("input:radio[name=Q4]:checked").val());
-    var display=parseInt( shortform + js + language + mwas )
+    var display=parseInt( shortform + java + language + mwas )
     var name=$("input#firstName").val();
     event.preventDefault();
     $("#hide").slideDown();
