8a5f8c8e3ab2f5b8cf8f9132da5a68dd8fdb8a53
ngishjonathan <balozimwash@gmail.com>
2019-04-01 15:04:47 +0300

    add jQuery function

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#add").submit(function(event) {
+    var shortform=parseInt($("input:radio[name=Q1]:checked").val());
+    var js=parseInt($("input:radio[name=Q2]:checked").val());
+    var language=parseInt("input:radio[name=Q3]:checked").val());
+    var mwas=parseInt("input:radio[name=Q4]:checked").val());
+    var display=parseInt( shortform + js + language + mwas )
+    var name=$("input#firstName").val();
+    event.preventDefault();
+    $("#hide").slideDown();
+    $("#results").text( firstName + "your score is: " + display + "%")
+  });
+});
