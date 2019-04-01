a55af533153c9ff889b957bf87870c09108be84a
ngishjonathan <balozimwash@gmail.com>
2019-04-01 10:47:07 +0300

    add jquery styles

--- QuizeBoard/js/scripts.js
+++ QuizeBoard/js/scripts.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-$("form#add").submit(function(event) {
-event.preventDefault();
-var Q1 = $("input:radio[name=Q1]:checked").val();
-var Q2 = $("input:radio[name=Q2]:checked").val();
-var Q3 = $("input:radio[name=Q3]:checked").val();
-var Q4 = $("input:radio[name=Q4]:checked").val();
-var result = add(Q1, Q2, Q3, Q4);
-$("#output").text(result);
-});
+  $(".js-hidden").click(function() {
+    $(".js-showing").toggle();
+    $(".js-hidden").toggle();
+  });
 });
